165d087b0b67d44ccc5be2ca82523a887441d174
bv-hemant <85125871+bv-hemant@users.noreply.github.com>
2021-06-03 16:38:25 +0530

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ load "linked_list.rb"
 different_data_structure = {bst: 0, linked_list: 1}
 
 puts	"#{different_data_structure.fetch(:bst)}: Binary search tree" ,
-			"#{different_data_structure.fetch(:linked_list)}: Linked List  "
+	"#{different_data_structure.fetch(:linked_list)}: Linked List  "
 
 print "enter your choice : "
 type_of_ds = gets
